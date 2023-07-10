394de6d6cb6238cb530a97664b23d82078f8343d
yllensc <yllensc18@gamil.com>
2023-07-10 17:27:10 -0500

    feat: Api de comida
    Restaurante para simular un menú y agregar al carrito lso pedidos

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const catBreakfast= "https://www.themealdb.com/api/json/v1/1/list.php?c=breakfast";
+const catChicken = "https://www.themealdb.com/api/json/v1/1/list.php?c=chicken";
+const catDessert = "https://www.themealdb.com/api/json/v1/1/list.php?c=dessert";
+const catPasta = "https://www.themealdb.com/api/json/v1/1/list.php?c=pasta";
+const catVegan = "https://www.themealdb.com/api/json/v1/1/list.php?c=vegetarian";
+const catSeaFood = "https://www.themealdb.com/api/json/v1/1/list.php?c=seafood";
+
